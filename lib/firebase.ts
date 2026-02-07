fa205b6c1ff4765ec6ee0068ff0ad2a5956d41eb
Harsh Jain <harshjain@uic.edu>
2026-02-07 12:59:49 -0600

    Use correct Firebase storage bucket

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -11,7 +11,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDdfT_No8cP1WfGNWKp7Rpoddp21fDtYqQ",
   authDomain: "sanghavi-gold.firebaseapp.com",
   projectId: "sanghavi-gold",
-  storageBucket: "sanghavi-gold.appspot.com", 
+  storageBucket: "sanghavi-gold.firebasestorage.app", 
   messagingSenderId: "1096140451138",
   appId: "1:1096140451138:web:49f0ce56639f7c58cf6cdb",
   measurementId: "G-71JY96ZQLP"
